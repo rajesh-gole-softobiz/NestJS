5edb0abc01878541e6d62edab103c4c3636503d7
rajesh-gol-softobiz <rajesh.gole@softobiz.com>
2022-08-04 19:45:44 +0530

    for different controller, create new obj for every service

--- controller-test/src/app.controller.ts
+++ controller-test/src/app.controller.ts
@@ -1,33 +1,38 @@
 /* eslint-disable prettier/prettier */
 import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { BookService } from "./book.service";
 
 
 
 @Controller('book')
 export class BookController{
+    public bookService: BookService = new BookService();
 
     //add book
     @Post('/add')
     addBook() : string{
-        return "This will add book"
+       return this.bookService.addBook();
     }
 
     //delete book
     @Delete('/delete')
     deleteBook() : string{
-        return "This will delete book"
+       return this.bookService.deleteBook();
+
     }
 
     //update book
     @Put('/update')
     updateBook() : string{
-        return "This will update book"
+       return this.bookService.updateBook();
+
     }
 
     @Get('/findAll')
     //find all book
     findAllsBook() : string{
-        return "This will find all books"
+       return this.bookService.findBook();
+
     }
 
     //Create Dynamic Route - https://docs.nestjs.com/controllers
---
+++ controller-test/src/book.service.ts
@@ -0,0 +1,21 @@
+/* eslint-disable prettier/prettier */
+
+
+export class BookService{
+
+    addBook(): string{
+        return 'This will add book';
+    }
+
+    deleteBook(): string{
+        return 'This will delete book';
+    }
+
+    updateBook(): string{
+        return 'This will update book';
+    }
+
+    findBook(): string{
+        return 'This will find book';
+    }
+}
